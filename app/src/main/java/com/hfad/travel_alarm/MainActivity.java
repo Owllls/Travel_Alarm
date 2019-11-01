27f971869f45241cb165f4c24d08f2d58a9b0672
pavel <pavelmazalov@outlook.com>
2019-11-01 03:39:08 +0200

    first steps

--- app/src/main/java/com/hfad/travel_alarm/MainActivity.java
+++ app/src/main/java/com/hfad/travel_alarm/MainActivity.java
@@ -1,6 +1,9 @@
 package com.hfad.travel_alarm;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout main_layout = (DrawerLayout) findViewById(R.id.main_layout);
+        ActionBarDrawerToggle open_panel_B = new ActionBarDrawerToggle(this,main_layout,toolbar,R.string.open_menu,R.string.set_alarm);
+
+        main_layout.addDrawerListener(open_panel_B);
+        open_panel_B.syncState();
     }
 }
